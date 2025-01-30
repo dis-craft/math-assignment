fee7e5eddecc72ef9b748f26453f4db7f181e6a3
dis-craft <mrsrikart@gmail.com>
2025-01-30 23:10:31 +0530

    added my first stream lit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,23 @@
-print("Hello world")
-x = int(input("Enter a number!"))
-print(x)
+# pip install -U streamlit
+# streamlit run streamlit_app.py
+
+import streamlit as st
+
+st.title("This is a title")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("Text data")
+# st.tk.Checkbutton(root, text="HELLO")
+st.write("This is a default font")
+st.markdown("This a markdown!")
+
+st.markdown("# Markdown heading")
+st.markdown("### Markdown heading")
+st.markdown("**Markdown heading in bold**")
+st.markdown("""`code Hello world 
+hello again` """)
+st.markdown('''
+1. First item
+2. Second item
+3. Third item
+            ''')
